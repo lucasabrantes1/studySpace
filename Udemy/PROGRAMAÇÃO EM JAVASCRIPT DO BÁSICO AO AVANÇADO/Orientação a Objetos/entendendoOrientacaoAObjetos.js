1341f42bcdb0ec1a53ec56d1af0bb4f282a76961
lucasabrantes002 <lucasabrantes002@gmail.com>
2021-12-25 14:54:17 -0300

    Entendendo a orientação a objetos

---
+++ Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/Orientação a Objetos/entendendoOrientacaoAObjetos.js
@@ -0,0 +1,35 @@
+/*
+    Orientação objetos nada mais é do que um paradigma de programação 
+    Paradigma = forma de programação, uma forma de se programar, o que ira impactar na forma que o software é desenvolvido'    
+*/
+
+/*
+    Elementos de POO
+
+    Classe = nada mais é do que um molde, aquilo que ira modelar um objeto na POO, é através desssa classe que os objetos serão criados 
+    no final. E essas classes vão sendo criadas para modelar os elementos do sistema que criamos. Por exemplo em um sistema de bliblioteca
+    devemos alencar as entidades, dessa bliblioteca como por exemplo os Livros possivelmente seria uma classe de livros para que todos os livros podessem ser cadastrados
+    e as blibliotecarias, haveria uma classe para mapear elas. Mesma coisa dos usúarios, onde possivelmente haverá uma classe para informações e ações desses usúarios.
+    
+    
+    Exemplo 2:
+
+    Classe Produto
+     nome
+     preco  <- atriubutos(caracteristicas do objeto que estamos moldado no nosso sismtema.)
+     desconto
+
+
+     Objetos instanciados   <-(Apartir que temos uma classe podemos criar instancias ex abaixo)
+
+     Notebook
+     R$2.3000
+     15%
+    
+     Apartir da classe podemos criar varias instancias.
+     
+     Caneta
+     R$2.94
+     5%
+
+*/
