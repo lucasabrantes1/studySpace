5a9148e826ca272144cedfe495319c7563446dac
lucasabrantes002 <lucasabrantes002@gmail.com>
2021-12-25 15:00:01 -0300

    Declarando Classes

---
+++ Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/Orientação a Objetos/declarandoClasses.js
@@ -0,0 +1,12 @@
+/*Classes São modelos de dados e apartir das classes que no final podemos isntanciar e criar objetos, em js basta utilizar a palavra class e definir o nome da class 
+conforme a baixo (AS CLASSES DEVEM INICIAR COM LETRA MAIUSCULAS ou  PessoaFisica se tiver mais de uma especificação  no nome)*/
+
+class Pessoa{
+
+}
+
+class Carro{
+
+}
+
+const p1 = new Pessoa(); //Estamos definindo o tipo de dado p1 como pessoa, porque estamos criando um modelo de dados.
