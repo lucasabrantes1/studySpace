182bc8c3303fe2a082e19cacbae635a0538ed002
lucasabrantes002 <lucasabrantes002@gmail.com>
2021-12-31 17:20:55 -0300

    Iniciando a aplicação

---
+++ Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/PRojeto JavaScript 1/js/script.js
@@ -0,0 +1,25 @@
+// 1 referenciar o input
+let input = document.querySelector("input[name=tarefa]");
+// 2 referenciar o button
+let btn = document.querySelector("#botao");
+// 3 referenciar a lista
+let lista = document.querySelector("#lista");
+
+
+let tarefas = [
+    'Jogar GTA1',
+    'Jogar GTA2',
+    'Jogar GTA3',
+    'Jogar GTA4',
+    'Jogar GTA5',
+    'Jogar GTA6',
+]
+
+
+/* <li class="list-group-item list-group-item-action">Jogar GTA5</li>
+<li class="list-group-item list-group-item-action">Jogar GTA5</li>
+<li class="list-group-item list-group-item-action">Jogar GTA5</li>
+<li class="list-group-item list-group-item-action">Jogar GTA5</li>
+<li class="list-group-item list-group-item-action">Jogar GTA5</li>
+<li class="list-group-item list-group-item-action">Jogar GTA5</li>
+*/
