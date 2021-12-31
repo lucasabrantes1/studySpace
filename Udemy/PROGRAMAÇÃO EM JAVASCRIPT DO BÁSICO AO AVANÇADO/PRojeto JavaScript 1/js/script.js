1a45d40d8d87cbd243b7cc5150b685c1ada65707
lucasabrantes002 <lucasabrantes002@gmail.com>
2021-12-31 17:47:43 -0300

    Inserindo dados na aplicação

--- Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/PRojeto JavaScript 1/js/script.js
+++ Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/PRojeto JavaScript 1/js/script.js
@@ -4,44 +4,80 @@ let input = document.querySelector("input[name=tarefa]");
 let btn = document.querySelector("#botao");
 // 3 referenciar a lista
 let lista = document.querySelector("#lista");
-
+//4 card
+let card = document.querySelector('.card')
 
 let tarefas = [
-    'Jogar GTA1',
-    'Jogar GTA2',
-    'Jogar GTA3',
-    'Jogar GTA4',
-    'Jogar GTA5',
-    'Jogar GTA6',
-]
+  "Jogar GTA1",
+  "Jogar GTA2",
+  "Jogar GTA3",
+  "Jogar GTA4",
+  "Jogar GTA5",
+  "Jogar GTA6",
+];
 
-function renderizarTarefas(){
-    for(tarefa of tarefas){
-        //Criar o item da lista
-        let itemLista = document.createElement('li')
+function renderizarTarefas() {
+  //Limpar a listagem de itens antes de renderizar novamente a tela
+  lista.innerHTML = "";
+  for (tarefa of tarefas) {
+    //Criar o item da lista
+    let itemLista = document.createElement("li");
 
-        //Adicionar classes no item da lista
-        itemLista.setAttribute('class', 'list-group-item list-group-item-action')
+    //Adicionar classes no item da lista
+    itemLista.setAttribute("class", "list-group-item list-group-item-action");
 
-        //Criar um texto
-        let itemTexto = document.createTextNode(tarefa)
-        
-        //Adicionar o texto no item da lista
-        itemLista.appendChild(itemTexto)
+    //Criar um texto
+    let itemTexto = document.createTextNode(tarefa);
 
-        //Adicionar o item da lista na lista
-        lista.appendChild(itemLista)
+    //Adicionar o texto no item da lista
+    itemLista.appendChild(itemTexto);
 
+    //Adicionar o item da lista na lista
+    lista.appendChild(itemLista);
 
-    }
+    //Limpar o input
+    input.value = "";
+  }
 }
 //Executando a função para renderizar as tarefas
-renderizarTarefas()
+renderizarTarefas();
 
-/* <li class="list-group-item list-group-item-action">Jogar GTA5</li>
-<li class="list-group-item list-group-item-action">Jogar GTA5</li>
-<li class="list-group-item list-group-item-action">Jogar GTA5</li>
-<li class="list-group-item list-group-item-action">Jogar GTA5</li>
-<li class="list-group-item list-group-item-action">Jogar GTA5</li>
-<li class="list-group-item list-group-item-action">Jogar GTA5</li>
-*/
+//1) Escutar o evento de clique no botão
+btn.onclick = function () {
+  //2) Capturar o valor digitado pelo usuário no input
+  let novaTarefa = input.value;
+
+  if (novaTarefa !== "") {
+    //3) Precisamos atualizar a nova tarefa na lista (array) de tarefas e renderizar a tela
+    tarefas.push(novaTarefa);
+
+    //4) Executando a função para renderizar as tarefas
+    renderizarTarefas();
+
+    //Limpa o input
+    input.value = "";
+
+    //Limpar mensagens de erro(spans)
+    removerSpans();
+  } else {
+    //Limpar mensagens de erro(spans)
+    removerSpans();
+
+    let span = document.createElement("span");
+    span.setAttribute("class", "alert alert-warning");
+
+    let msg = document.createTextNode("Você precisa informar a tarefa!");
+
+    span.appendChild(msg);
+
+    card.appendChild(span);
+  }
+};
+
+function removerSpans() {
+  let spans = document.querySelectorAll("span");
+
+  for (let i = 0; i < spans.length; i++) {
+    card.removeChild(spans[i]);
+  }
+}
