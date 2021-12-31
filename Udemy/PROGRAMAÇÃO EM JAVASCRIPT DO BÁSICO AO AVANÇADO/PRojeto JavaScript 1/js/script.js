d7b3dcbe592fb10ca7daee431f40dc78ef08b02e
lucasabrantes002 <lucasabrantes002@gmail.com>
2021-12-31 17:27:14 -0300

    Renderizando a aplicação

--- Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/PRojeto JavaScript 1/js/script.js
+++ Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/PRojeto JavaScript 1/js/script.js
@@ -15,6 +15,28 @@ let tarefas = [
     'Jogar GTA6',
 ]
 
+function renderizarTarefas(){
+    for(tarefa of tarefas){
+        //Criar o item da lista
+        let itemLista = document.createElement('li')
+
+        //Adicionar classes no item da lista
+        itemLista.setAttribute('class', 'list-group-item list-group-item-action')
+
+        //Criar um texto
+        let itemTexto = document.createTextNode(tarefa)
+        
+        //Adicionar o texto no item da lista
+        itemLista.appendChild(itemTexto)
+
+        //Adicionar o item da lista na lista
+        lista.appendChild(itemLista)
+
+
+    }
+}
+//Executando a função para renderizar as tarefas
+renderizarTarefas()
 
 /* <li class="list-group-item list-group-item-action">Jogar GTA5</li>
 <li class="list-group-item list-group-item-action">Jogar GTA5</li>
