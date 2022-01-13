488ad6fc037b3d15f833033ea8a6f6e0e3c0a075
lucasabrantes002 <lucasabrantes002@gmail.com>
2022-01-12 21:11:15 -0300

    Configurando o repositório

--- Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/Projeto JavaScript 2/src/main.js
+++ Udemy/PROGRAMAÇÃO EM JAVASCRIPT DO BÁSICO AO AVANÇADO/Projeto JavaScript 2/src/main.js
@@ -1,1 +1,38 @@
-document.querySelector('body').style.background  = 'red'
+class App{
+
+
+    //Constructor
+    constructor(){
+        //Lista de repositorio
+        this.repositories = []
+
+        // Form
+        this.formulario = document.querySelector('form')
+
+        //Método para registrar os eventos
+        this.registrarEventos() 
+    }
+
+    registrarEventos(){
+        this.formulario.onsubmit = evento => this.adicionarRepositorio(evento)
+    }
+
+    adicionarRepositorio(evento){
+        //Evita que o formulário recarregue a página.
+        evento.preventDefault();
+        
+        //Adiciona o repositorio na lista.
+        this.repositories.push({
+            nome: "Nerd Fonts",
+            descricao: "Iconic font aggregatos, collection and patcher",
+            avatar_url: "http://avatars0.githubusercontent.com/u/8083459?v=4",
+            link: "https://github.com/ryanoasis/nerd-fonts"
+        })
+
+        //Renderizar a tela
+        console.log(this.repositories)
+    }
+
+}
+
+new App()
