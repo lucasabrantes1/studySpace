f24839a7ec5c728f3d0d1aa853f1307ce14f64e9
lucasabrantes002 <lucasabrantes002@gmail.com>
2022-01-18 18:11:22 -0300

    Tipos Numéricos e Booleano

--- Udemy/EntendendoTypeScript/usandoTipos/tipos/tipo.js
+++ Udemy/EntendendoTypeScript/usandoTipos/tipos/tipo.js
@@ -4,3 +4,12 @@
 let nome = 'João';
 console.log(nome);
 // nome = 28 --> não é possível inferir valor pois ambos são de diferentes tipos.
+//numbers
+let idade = 27;
+// idade = 'ana'
+idade = 27.5; //--> não faz destinção de pontos flutuantes e --> não é possível inferir valor pois ambos são de diferentes tipos.
+console.log(idade);
+//boolean 
+let possuiHobbies = false;
+// possuiHobbies = 1 --> não é possível inferir valor pois ambos são de diferentes tipos.
+console.log(possuiHobbies);
