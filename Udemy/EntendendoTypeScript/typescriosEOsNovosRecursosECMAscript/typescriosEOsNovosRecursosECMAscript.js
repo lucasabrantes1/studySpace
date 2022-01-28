b17f55947cddbaa51d81a57d4aa97dce9a3aae5b
lucasabrantes002 <lucasabrantes002@gmail.com>
2022-01-28 14:16:14 -0300

    Let e Const #01

--- Udemy/EntendendoTypeScript/typescriosEOsNovosRecursosECMAscript/typescriosEOsNovosRecursosECMAscript.js
+++ Udemy/EntendendoTypeScript/typescriosEOsNovosRecursosECMAscript/typescriosEOsNovosRecursosECMAscript.js
@@ -7,10 +7,26 @@ if (estaFrio) {
     let acao = 'Colocar o casado!';
     console.log(acao);
 }
-const cpf = '411.888.55.8';
-cpf = '44.44.4.4.4.';
-console.log(cpf);
+// const cpf: string = '411.888.55.8'
+// cpf = '44.44.4.4.4.'
+// console.log(cpf)
 //??Let e Const #01 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-//!!Arquivo de Saída --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-//!!Arquivo de Saída --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//!!Let e Const #01 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+/* Tanto let quanto const , tem escopo de bloco, quando em um bloco estara visivel somente la*/
+var segredo = 'externo';
+function revelar() {
+    const segredo = 'interno';
+    console.log(segredo);
+}
+revelar();
+console.log(segredo);
+{
+    const def = 'def';
+    console.log(def);
+}
+for (let i = 0; i < 0; i++) {
+    console.log(i);
+}
+// console.log(i) //n na pra acessar.
+//!!Let e Const #01 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //# sourceMappingURL=typescriosEOsNovosRecursosECMAscript.js.map
