010191d962f02a88b624c8f4176ddbfe4a8b90e1
lucasabrantes002 <lucasabrantes002@gmail.com>
2022-01-31 14:19:12 -0300

    Promises

--- Udemy/EntendendoTypeScript/typescriosEOsNovosRecursosECMAscript/typescriosEOsNovosRecursosECMAscript.js
+++ Udemy/EntendendoTypeScript/typescriosEOsNovosRecursosECMAscript/typescriosEOsNovosRecursosECMAscript.js
@@ -154,9 +154,34 @@ const notificacoes = '19';
 //Com template string
 const boasVindas = `
 
-    Boas vindas ${usuarioId},
-    Notificações: ${parseInt(notificacoes) > 9 ? '+9' : notificacoes},  
+Boas vindas ${usuarioId},
+Notificações: ${parseInt(notificacoes) > 9 ? '+9' : notificacoes},  
 `;
 console.log(boasVindas);
 //??Template String --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//!!Promises --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+/* Uma promessa algo que vai chegar no futuro, asycronismo(Async await) */
+// function esperar3s(callback: (dado: string) => void){
+//    setTimeout(() => {
+//       callback('3s depois...')
+//    }, 3000)
+// }
+// esperar3s(function(resultado: string){
+//    console.log(resultado)
+// })
+// function esperar3sPromise(){
+//    return new Promise((resolve: any) =>{
+//       setTimeout(() => {
+//          resolve('3s depois promise...')
+//       }, 3000)
+//    })
+// }
+// esperar3sPromise().then(dado => console.log(dado))
+fetch('https://swapi.dev/api/people/1')
+    .then(res => res.json())
+    .then(personagem => personagem.films)
+    .then(films => fetch(films[1]))
+    .then(resFilm => resFilm.json())
+    .then(filme => console.log(filme.title));
+//!!Promises --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //# sourceMappingURL=typescriosEOsNovosRecursosECMAscript.js.map
