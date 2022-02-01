af8a00210aa79cb0b2f771ca9f7f9ee0db3a59fb
lucasabrantes002 <lucasabrantes002@gmail.com>
2022-02-01 13:09:25 -0300

    Modificadores de Acesso

--- Udemy/EntendendoTypeScript/usandoClassesParaCriarObjetos/usandoclasseseobjetos.js
+++ Udemy/EntendendoTypeScript/usandoClassesParaCriarObjetos/usandoclasseseobjetos.js
@@ -69,4 +69,40 @@ console.log(prod1.resumo());
 const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
 console.log(prod2.resumo());
 //??Classes & Métodos --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//!!Modificadores de Acesso  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+/*private, public e protected*/
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('Ford', 'Ka', 185);
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+Array(25).fill(0).forEach(() => carro1.frear());
+console.log(carro1.frear());
+//Simular erros
+//  carro1.velocidadeAtual = 300
+//  console.log('Atual ->' + carro1.velocidadeAtual)
+//!!Modificadores de Acesso  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //# sourceMappingURL=usandoclasseseobjetos.js.map
--- Udemy/EntendendoTypeScript/usandoClassesParaCriarObjetos/usandoclasseseobjetos.ts
+++ Udemy/EntendendoTypeScript/usandoClassesParaCriarObjetos/usandoclasseseobjetos.ts
@@ -70,7 +70,7 @@ OBS2 - CRIAR DOIS PRODUTOS: PASSANDO DOIS E TRêS PARAMETROS
 //     const prod2 = new Produto ('Caderno Escolar', 18.80, 0.15)
 //     console.log(prod2)    
 //!!Desafio  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    
+
 
 
 //??Classes & Métodos --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -80,25 +80,79 @@ class Produto {
         public desconto: number = 0) {
             
         }
-
-                precoComDesconto(): number{
-                    return this.preco * (1 - this.desconto)
+        
+        precoComDesconto(): number{
+            return this.preco * (1 - this.desconto)
                 }
-
-
-      /* metodo */  public resumo(): string{
-                return `${this.nome} custa R$${this.precoComDesconto()} (${this.desconto * 100}% off)`
-        }
-
-
-    }
-    
-    const prod1 = new Produto('Caneta', 4.20)
-    prod1.desconto = 0.06
-    console.log(prod1.resumo())
-    
-    const prod2 = new Produto ('Caderno Escolar', 18.80, 0.15)
+                
+                
+                /* metodo */  public resumo(): string{
+                    return `${this.nome} custa R$${this.precoComDesconto()} (${this.desconto * 100}% off)`
+                }
+                
+                
+            }
+            
+            const prod1 = new Produto('Caneta', 4.20)
+            prod1.desconto = 0.06
+            console.log(prod1.resumo())
+            
+            const prod2 = new Produto ('Caderno Escolar', 18.80, 0.15)
     console.log(prod2.resumo())  
-
+    
 
 //??Classes & Métodos --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    
+    
+//!!Modificadores de Acesso  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+            /*private, public e protected*/
+
+     class Carro {
+         private velocidadeAtual: number = 0
+
+         constructor(public marca:string, public modelo: string,
+            private velocidadeMaxima: number = 200){
+
+            }
+
+            private alterarVelocidade(delta: number): number{
+                const novaVelocidade = this.velocidadeAtual + delta
+                const velocidadeValida =  novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima
+
+                if (velocidadeValida) {
+                    this.velocidadeAtual = novaVelocidade
+                } else {
+                    this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+                }
+
+                return this.velocidadeAtual
+            }
+
+            public acelerar(): number{
+                return this.alterarVelocidade(5)
+            }
+
+            public frear() {
+                return this.alterarVelocidade(-5)
+            }
+     }     
+     
+     const carro1 = new Carro('Ford', 'Ka', 185)
+     
+
+     Array(50).fill(0).forEach(() => carro1.acelerar())
+     console.log(carro1.acelerar())
+
+     
+     Array(25).fill(0).forEach(() => carro1.frear())
+     console.log(carro1.frear())
+
+
+     //Simular erros
+
+    //  carro1.velocidadeAtual = 300
+    //  console.log('Atual ->' + carro1.velocidadeAtual)
+
+    
+//!!Modificadores de Acesso  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
