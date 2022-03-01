df4f6c848e5dbaa0f5613dd138aa2c4c693151a2
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-03-01 16:32:50 -0300

    Usando Interfaces com Classes

--- Udemy/EntendendoTypeScript/interface/interfaces.js
+++ Udemy/EntendendoTypeScript/interface/interfaces.js
@@ -7,10 +7,30 @@ function mudarNome(pessoa) {
 }
 const pessoa = {
     nome: 'Lucas',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
-saudarComOla({ nome: 'Jonas', idade: 27, xyz: true });
+// saudarComOla({nome: 'Jonas', idade: 27, xyz: true})
+pessoa.saudar('Skywalker');
+//usando classe
+class Cliente {
+    constructor() {
+        this.nome = '';
+        this.ultimaCompra = new Date;
+    }
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é ' + this.nome + ' ' + sobrenome);
+    }
+}
+const meuCliente = new Cliente();
+meuCliente.nome = 'Han';
+saudarComOla(meuCliente);
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
 //# sourceMappingURL=interfaces.js.map
--- Udemy/EntendendoTypeScript/interface/interfaces.ts
+++ Udemy/EntendendoTypeScript/interface/interfaces.ts
@@ -2,6 +2,7 @@ interface Humano {
     nome: string
     idade?: number
     [prop: string]: any
+    saudar(sobrenome: string): void
 }
 
 function saudarComOla(pessoa: Humano){
@@ -12,12 +13,33 @@ function mudarNome(pessoa: Humano){
     pessoa.nome = "Joana"
 }
 
-const pessoa = {
+const pessoa: Humano = {
     nome: 'Lucas',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome: string){
+        console.log('Olá, meu nome é '
+        + this.nome + ' ' + sobrenome)
+    }
 }
 
 saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
-saudarComOla({nome: 'Jonas', idade: 27, xyz: true})
+// saudarComOla({nome: 'Jonas', idade: 27, xyz: true})
+pessoa.saudar('Skywalker')
+
+
+//usando classe
+class Cliente implements Humano {
+    nome: string = ''
+    ultimaCompra: Date = new Date
+    saudar(sobrenome: string) {
+        console.log('Olá, meu nome é ' + this.nome + ' ' + sobrenome)
+    }
+}
+
+const meuCliente = new Cliente()
+meuCliente.nome = 'Han'
+saudarComOla(meuCliente)
+meuCliente.saudar('Solo')
+console.log(meuCliente.ultimaCompra)
