f8082acb4f9a1a4f40a6a91c496c3ddf42bbe837
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-03-05 16:27:35 -0300

    Por que usar

---
+++ Udemy/EntendendoTypeScript/generics/generics.js
@@ -0,0 +1,11 @@
+"use strict";
+//!!Classes & Atributos #01 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//??Reaseons to use --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// function echo(objeto: any){
+//     return objeto  
+// }
+// console.log(echo('Lucas').length)
+// console.log(25).length ---> a nomeclatura do generics deixaria de forma mais explicita esse erro.
+// console.log(echo({nome: 'lucas', idade: 27}))
+//??Reaseons to use --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//# sourceMappingURL=generics.js.map
