9a6107221ce9123383d02492cb640286c5a3b7e5
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-03-05 16:41:04 -0300

    Criando uma Função com Generics

--- Udemy/EntendendoTypeScript/generics/generics.js
+++ Udemy/EntendendoTypeScript/generics/generics.js
@@ -1,5 +1,4 @@
 "use strict";
-//!!Classes & Atributos #01 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //??Reaseons to use --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // function echo(objeto: any){
 //     return objeto  
@@ -8,4 +7,15 @@
 // console.log(25).length ---> a nomeclatura do generics deixaria de forma mais explicita esse erro.
 // console.log(echo({nome: 'lucas', idade: 27}))
 //??Reaseons to use --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//!!Criando uma Função com Generics --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//Para utilizar um tipo genérico primeiro deve se definir o <T> o T poderia ser qualquer letra
+// mas indica tipo. Logo após fazer isso definimos que o objeto é do tipo T
+function echoDoDeCima(objeto) {
+    return objeto;
+}
+console.log(echoDoDeCima('Lucas').length);
+console.log(echoDoDeCima(25)); //Note que o t foi passado como paremtro de number aqui na pratica ele troca todos os t por number.
+console.log(echoDoDeCima({ nome: 'lucas', idade: 27 }));
+// Resumidamente o tipo é definido no momento da execução da função.
+//!!Criando uma Função com Generics --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //# sourceMappingURL=generics.js.map
--- Udemy/EntendendoTypeScript/generics/generics.ts
+++ Udemy/EntendendoTypeScript/generics/generics.ts
@@ -1,4 +1,4 @@
-//!!Classes & Atributos #01 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
 //??Reaseons to use --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // function echo(objeto: any){
 //     return objeto  
@@ -8,3 +8,20 @@
 // console.log(echo({nome: 'lucas', idade: 27}))
 
 //??Reaseons to use --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+//!!Criando uma Função com Generics --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    //Para utilizar um tipo genérico primeiro deve se definir o <T> o T poderia ser qualquer letra
+    // mas indica tipo. Logo após fazer isso definimos que o objeto é do tipo T
+
+    function echoDoDeCima<T>(objeto: T): T{
+        return objeto
+    }
+
+    console.log(echoDoDeCima('Lucas').length)
+    console.log(echoDoDeCima<number>(25))  //Note que o t foi passado como paremtro de number aqui na pratica ele troca todos os t por number.
+    console.log(echoDoDeCima({nome: 'lucas', idade: 27}))
+
+
+    // Resumidamente o tipo é definido no momento da execução da função.
+//!!Criando uma Função com Generics --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
