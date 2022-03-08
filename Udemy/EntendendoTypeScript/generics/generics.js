329a56027cadb6ce8fdfeeade565eb95409bd0ae
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-03-08 19:17:12 -0300

    Criando Classes com Generics #03

--- Udemy/EntendendoTypeScript/generics/generics.js
+++ Udemy/EntendendoTypeScript/generics/generics.js
@@ -53,4 +53,22 @@ class SomaBinaria extends OperacaoBinaria {
 }
 console.log(new SomaBinaria(3, 4).executar());
 //??Criando Classes com Generics #02 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//!!Criando Classes com Generics #03 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+class DiferencaEntreDatas extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+}
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(5, 5, 2022);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
+//!!Criando Classes com Generics #03 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //# sourceMappingURL=generics.js.map
