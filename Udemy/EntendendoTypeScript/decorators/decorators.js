b61b04cb32bc597e566e4f88e016c17ccf8cda9b
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-03-10 19:51:56 -0300

    Alterando Construtor com Decorator de Classe

--- Udemy/EntendendoTypeScript/decorators/decorators.js
+++ Udemy/EntendendoTypeScript/decorators/decorators.js
@@ -8,13 +8,40 @@
 //         console.log('Novo...')
 //     }
 // }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 // function logarClasse(construtor: Function){
 //     console.log(construtor)
 // }
-//!!Criando Decorator de Classe --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //??Decorator Factory --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // function logarCLaseSe(valor: boolean){
 //     return valor ? logarClasse : null
 // }
 //??Decorator Factory --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//!!Alterando Construtor com Decorator de Classe --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// @logarClasseSe(true)
+let Eletrodomestico = class Eletrodomestico {
+    constructor() {
+        console.log('Novo...');
+    }
+};
+Eletrodomestico = __decorate([
+    logarObjeto
+], Eletrodomestico);
+function logarObjeto(construtor) {
+    console.log('Carregado...');
+    return class extends construtor {
+        constructor(...args) {
+            console.log('Antes...');
+            super(...args);
+            console.log('Depois...');
+        }
+    };
+}
+new Eletrodomestico();
+//!!Alterando Construtor com Decorator de Classe --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //# sourceMappingURL=decorators.js.map
--- Udemy/EntendendoTypeScript/decorators/decorators.ts
+++ Udemy/EntendendoTypeScript/decorators/decorators.ts
@@ -1,3 +1,6 @@
+
+
+
 //!!Criando Decorator de Classe --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // /*O decorator é uma função, e essa função*/
 // @logarClasseSe(false)
@@ -8,19 +11,48 @@
 //     }
 // }
 
+
 // function logarClasse(construtor: Function){
 //     console.log(construtor)
 // }
 
 
-//!!Criando Decorator de Classe --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
 //??Decorator Factory --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 // function logarCLaseSe(valor: boolean){
-//     return valor ? logarClasse : null
-// }
-
-
+    //     return valor ? logarClasse : null
+    // }
+    
+    
 //??Decorator Factory --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+//!!Alterando Construtor com Decorator de Classe --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+// @logarClasseSe(true)
+@logarObjeto
+class Eletrodomestico {
+    constructor(){
+        console.log('Novo...')
+    }
+}
+
+
+type Construtor = {new(...args: any[]): {}}
+
+function logarObjeto(construtor: Construtor){
+    console.log('Carregado...')
+    return class extends construtor {
+        constructor(...args: any[]) {
+            console.log('Antes...')
+            super(...args)
+            console.log('Depois...')
+        }
+    }
+}
+
+new Eletrodomestico()
+
+//!!Alterando Construtor com Decorator de Classe --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
