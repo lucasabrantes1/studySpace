3d63e0d0750b622bb3c7e71f2b14651ef6fc493d
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-03-24 20:03:55 -0300

    Final integração gulp

--- Udemy/EntendendoTypeScript/integracao-gulp/gulpfile.js
+++ Udemy/EntendendoTypeScript/integracao-gulp/gulpfile.js
@@ -3,23 +3,38 @@ const del = require('del')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
 const tsify = require('tsify')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
 
-
-
-function limparDist(cb){
-   cb() 
+function limparDist() {
+    return del(['dist'])
 }
 
-
-function copiarHTML(cb){
-    cb()
+function copiarHTML(cb) {
+    return src('public/**/*')
+        .pipe(dest('dist'))
 }
 
-function gerarJS(cb){
-    cb()
+function gerarJS(cb) {
+    return browserify({
+        basedir: '.',
+        entries: ['src/main.ts']
+    })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(dest('dist'))
+}
+
+function gerarJSProducao() {
+    return src('dist/app.js')
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(dest('dist'))
 }
 
 exports.default = series(
     limparDist,
-    parallel(gerarJS, copiarHTML)
+    parallel(gerarJS, copiarHTML),
+    gerarJSProducao
 )
