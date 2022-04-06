bc8e3f0b1eb0161452850ae27038343bb184a533
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-04-06 17:14:13 -0300

    Servindo HTML estático

--- RocketSeat/ReactJS/chapter1/01-github-explore/webpack.config.js
+++ RocketSeat/ReactJS/chapter1/01-github-explore/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');	
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports =  {
     mode: 'development',
@@ -11,6 +11,11 @@ module.exports =  {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
