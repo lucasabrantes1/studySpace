52c2a8b55285faa3fdc10cd2625b9499dad9f1d4
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-04-06 17:53:50 -0300

    Webpack Dev Server

--- RocketSeat/ReactJS/chapter1/01-github-explore/webpack.config.js
+++ RocketSeat/ReactJS/chapter1/01-github-explore/webpack.config.js
@@ -11,6 +11,9 @@ module.exports =  {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
