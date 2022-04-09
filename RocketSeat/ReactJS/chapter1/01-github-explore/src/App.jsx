5eb55b95b4631572ff81ab5ad564731b43b95a40
lucasabrantes1 <lucasabrantes002@gmail.com>
2022-04-09 12:24:58 -0300

    Primeiro componente React

--- RocketSeat/ReactJS/chapter1/01-github-explore/src/App.jsx
+++ RocketSeat/ReactJS/chapter1/01-github-explore/src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <h1>Hello ReactJS</h1>
+    return <RepositoryList/>
 }
