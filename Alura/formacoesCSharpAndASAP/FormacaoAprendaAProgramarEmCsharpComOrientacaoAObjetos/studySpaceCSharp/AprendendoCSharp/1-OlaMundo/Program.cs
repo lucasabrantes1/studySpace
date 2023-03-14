40406828cf696500dd10669635a225a19a14e364
lucasabrantes1 <lucasabrantes002@gmail.com>
2023-03-14 19:43:49 -0300

    Formação asapc stopped

---
+++ Alura/formacoesCSharpAndASAP/FormacaoAprendaAProgramarEmCsharpComOrientacaoAObjetos/studySpaceCSharp/AprendendoCSharp/1-OlaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+ class Programa 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tecle ente para fechar ...");
+        Console.ReadLine();
+        
+    }
+}
+
