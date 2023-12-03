d410fe10a9315c713d0a4c356355c7cc359b2cdb
Abrantes <lucas.abrantes@kuehne-nagel.com>
2023-12-03 19:28:01 -0300

    Adiciona restante

--- Alura/formacoesCSharpAndASAP/FormacaoAprendaAProgramarEmCsharpComOrientacaoAObjetos/CriandoSuaPrimeiraAplicacao/execicios.cs
+++
@@ -1,9 +0,0 @@
-﻿//aa
-// Crie uma lista de inteiros utilizando o List<int> e utilize o for e o foreach para iterar sobre essa lista e exibir apenas os números pares.
-
-// No for, comece o índice em 0 e incremente a cada iteração. Defina a condição de continuação do loop como i < numeros.Count, ou seja, o loop continuará enquanto o valor de i for menor do que o número de elementos na lista. Acesse o valor de cada elemento utilizando a sintaxe numeros[i].
-
-// Já no foreach, ao invés de utilizar um loop for com um contador i e acessar os elementos da lista através do índice i, é utilizado o foreach para percorrer a lista e acessar diretamente cada elemento com a variável número. Em seguida, é verificado se o número é par utilizando o operador de módulo (%) e, caso seja, o número é impresso no console.
-
-
-List<string> listaDasBandas = new List<string>{"U2","The Beatles","Rammstein"};
