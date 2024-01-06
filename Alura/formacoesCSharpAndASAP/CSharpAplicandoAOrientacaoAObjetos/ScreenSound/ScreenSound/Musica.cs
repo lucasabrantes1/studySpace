c101c11111d78f62448abde318ef0269eab6b710
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-01-06 16:34:41 -0300

    props

--- Alura/formacoesCSharpAndASAP/CSharpAplicandoAOrientacaoAObjetos/ScreenSound/ScreenSound/Musica.cs
+++ Alura/formacoesCSharpAndASAP/CSharpAplicandoAOrientacaoAObjetos/ScreenSound/ScreenSound/Musica.cs
@@ -1,16 +1,21 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string Nome {  get; set; }
+
+    public string Artista { get; set; }
+
+    public int Duracao { get; set; }
+
+    public bool Disponivel { get; set; }
+
+    public string NomeCompleto { get; set; }
 
     public void ExibirFichaTecnica(){
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
-        Console.WriteLine($"Disponível: {disponivel}");
-        if(disponivel == true){
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        Console.WriteLine($"Disponível: {Disponivel}");
+        if(Disponivel == true){
             Console.WriteLine("Disponível no plano\n");
         }
         else { Console.WriteLine("Adquira o plano Plus+\n");
--- Alura/formacoesCSharpAndASAP/CSharpAplicandoAOrientacaoAObjetos/ScreenSound/ScreenSound/Program.cs
+++ Alura/formacoesCSharpAndASAP/CSharpAplicandoAOrientacaoAObjetos/ScreenSound/ScreenSound/Program.cs
@@ -1,29 +1,18 @@
-﻿//Musica musica1 = new Musica();
-//musica1.nome = "Ego brain";
-//musica1.artista = "System of a down";
-//musica1.duracao = 273;
-//musica1.disponivel = true;
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Ego brain";
+musica1.Artista = "System of a down";
+musica1.Duracao = 273;
+musica2.Disponivel = true;
+Console.WriteLine(musica1.disponivel);
 
-//Musica musica2 = new Musica();
-//musica2.nome = "Vertigo";
-//musica2.artista = "U2";
-//musica2.duracao = 367;
-//musica2.disponivel = false;
-
-
-//musica1.ExibirFichaTecnica();
-//musica2.ExibirFichaTecnica();
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
 
 
-//ContaBancaria conta1 = new ContaBancaria();
-//conta1.numeroIndicador = 154646;
-//conta1.titular = "Lucas";
-//conta1.saldo = 22000000d;
-//conta1.senha = "123";
-//conta1.ExibirContaBancaria();
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
 
-
-Carro carro1 = new Carro();
-carro1.frear();
-carro1.buzinar();
-carro1.acelerar();
+Console.ReadLine();
