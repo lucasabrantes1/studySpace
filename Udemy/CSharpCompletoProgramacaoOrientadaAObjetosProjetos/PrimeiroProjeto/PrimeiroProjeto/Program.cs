b2bba26011d3717d9761d9cde087bc4973f3942b
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-01-18 21:56:19 -0300

    Classes poo

---
+++ Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior area: X");
+            }
+            else {
+                Console.WriteLine("Maior ");
+            }
+        }
+    }
+}
---
+++ Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/PrimeiroProjeto/PrimeiroProjeto/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimeiroProjeto {
+    internal class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+ 
+    }
+}
