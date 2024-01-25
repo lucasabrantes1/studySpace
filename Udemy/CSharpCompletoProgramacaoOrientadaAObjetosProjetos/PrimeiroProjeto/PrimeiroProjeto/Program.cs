53f685fcfd251946989fd4e2e32997a1cab47646
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-01-25 16:45:49 -0300

    a

--- Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -40,4 +40,6 @@ namespace PrimeiroProjeto {
 
         }
     }
+
+
 }
--- Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
+++ Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2bba26011d3717d9761d9cde087bc4973f3942b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e579e131314cd1fd30391beefc18cded31cb9cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
