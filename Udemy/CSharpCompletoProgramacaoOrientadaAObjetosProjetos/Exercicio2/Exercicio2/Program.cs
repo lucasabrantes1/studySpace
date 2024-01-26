0959d4cce7b099a19926d11b5561e7d559deb22e
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-01-26 20:07:40 -0300

    Exercicio aluno notas

---
+++ Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+            Funcionario funcionario = new Funcionario();
+
+             Console.Write("Nome: ");
+             funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salario bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(funcionario);
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine("Dados atualizados: " + funcionario);
+
+        }
+    }
+
+}
---
+++ Udemy/CSharpCompletoProgramacaoOrientadaAObjetosProjetos/Exercicio3/Exercicio3/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3 {
+    internal class Aluno {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado() {
+            if (NotaFinal() >= 60.0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public double NotaRestante() {
+            if (Aprovado()){
+                return 0.0;
+            } else {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
