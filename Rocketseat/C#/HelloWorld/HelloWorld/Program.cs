c6ada873d9ced9a1b57f861fe1f8eb9eab54244f
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-10-05 14:22:48 -0300

    Enum

--- Rocketseat/C#/HelloWorld/HelloWorld/Program.cs
+++ Rocketseat/C#/HelloWorld/HelloWorld/Program.cs
@@ -4,32 +4,19 @@ using System.Text;
 namespace HelloWorld;
 
 class Program
-{
+{   
+    enum NivelDeDificuldade
+    {
+        Baixo = 0, 
+        Medio = 1,
+        Alto = 2
+    }
     static void Main()
     {
-       DateOnly dia = new DateOnly(2023, 12, 1);
+        NivelDeDificuldade nivel = NivelDeDificuldade.Alto;
+        Console.WriteLine(nivel);
 
-       string diaEmTexto = dia.ToLongDateString();
-        //Console.WriteLine(diaEmTexto);
-
-
-       string diaEmTextoTraduzido = dia.ToString("", new CultureInfo("pt-BR"));
-       string diaEmTextoTraduzidoFormatado1 = dia.ToString("d MMMM yyyy", new CultureInfo("pt-BR"));
-       string diaEmTextoTraduzidoFormatado2 = dia.ToString("d 'de' MMMM 'de' yyyy", new CultureInfo("pt-BR"));
-        //Console.WriteLine(diaEmTextoTraduzidoFormatado2);
-
-        DateTime dia1 = new DateTime(2023, 12, 1, 20, 07, 1);
-        DateTime agora = DateTime.Now;
-        DateTime hoje = DateTime.Today;
-        DateTime hojeUTC = DateTime.UtcNow;
-        DateTime novaData = hoje.AddDays(1);
-
-        //Console.WriteLine(dia1);
-        //Console.WriteLine(agora);
-        //Console.WriteLine(hojeUTC);
-        Console.WriteLine(hoje);
-        Console.WriteLine(novaData);
-
-
+        int nivelInteiro = (int)nivel;
+        Console.WriteLine(nivelInteiro);
     }
 }
--- Rocketseat/C#/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Rocketseat/C#/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ec270d789b82a2de27f1c3f4f19a02598ac8a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6be0be05bda9e1b06583aab5dcc4a004375d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
