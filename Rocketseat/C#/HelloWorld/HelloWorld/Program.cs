86d6bb12a70f4813013dfe9e4e58d6965943c1a3
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-10-19 14:27:33 -0300

    If ternÃario

--- Rocketseat/C#/HelloWorld/HelloWorld/Program.cs
+++ Rocketseat/C#/HelloWorld/HelloWorld/Program.cs
@@ -5,154 +5,39 @@ class Program
 {
     static void Main()
     {
-        //int numero = 0;
-        //double saldo = 100.50;
-        //bool ativo = false;
-        //string autor = "Lucas";
-        //List<int> lista = new List<int> { 1, 7 };
-        //Cor cor = Cor.Amarelo;
-
-        /*
-            > Maior
-            < Menor
-            >= Maior ou igual
-            <= Menor ou igual
-            != diferente
-            == 
-        */
 
 
+        // if normal
+        //int numero = 7;
+        //string autor;
 
-        /// NUMEROS INTEIROS
-        //if (numero > 0)
+
+        //if (numero == 7)
         //{
-        //    Console.WriteLine("Este numero e POSITIVO");
-        //}
-        //else if (numero == 0)
-        //{
-        //    Console.WriteLine("ESTE numero e NEUTRO");
+        //    autor = "Lucas";
         //}
         //else
         //{
-        //    Console.WriteLine("Este numero e NEGATIVO");
+        //    autor = "Ouken";
         //}
 
-
-        // NUMEROS BOLEANOS
-        //if (saldo > 100.0)
-        //{
-        //    Console.WriteLine("Este numero e POSITIVO");
-        //}
-        //else if (saldo == 0.0)
-        //{
-        //    Console.WriteLine("ESTE numero e NEUTRO");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Este numero e NEGATIVO");
-        //}
+        //Console.WriteLine(autor);
 
 
-        //VALORES BOLEANOS, pra condicionasi boleanas n precisa setar o == para verificar se e true  so colocar a variavel
-        // O ponto de exclamação inveter a lógica do booleano setando para false
-        //if (!ativo)
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //} else
-        //{
-        //    Console.WriteLine("Login desativado entrar em contato com a moderação!");
-        //}
-
-
-        //String
-        // O ponto de exclamação inveter a lógica da string ou seja o que nao for igual 
-        //if (!autor.Equals("Lucas"))
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Login errado entrar em contato com a moderação!");
-        //}
-
-
-
-        //String
-        // O ponto de exclamação inveter a lógica da string ou seja o que nao for igual 
-        //if (!autor.Equals("Lucas"))
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Login errado entrar em contato com a moderação!");
-        //}
-
-
-        //Lista
-        // O ponto de exclamação inveter a lógica da string ou seja o que nao for igual 
-        //if (lista.Count > 0)
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //}
-
-
-        //if (lista.Contains(7))
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //}
-
-        //==PARTE 2
-
+        // if ternario so tem pra if e else
         int numero = 0;
-        double saldo = 100.50;
-        bool ativo = false;
-        string autor = "Lucas";
-        List<int> lista = new List<int> { 1, 7 };
-        Cor cor = Cor.Amarelo;
+        //na logica do ternario o ? indica o se(if) e o ; else
+        //na logica do ternario o ? indica o se(if) e o ; else
+        // se numero == 7 for verdade
+        //? entao faça iisso  Lucas
+        //: se nao Willian
+        string autor = numero == 7 ? "Lucas" : "Willian";
 
+        //com mais condicoes
+        string autor2 = numero == 7 || numero > 100 ? "Lucas" : "Willian";
+        string autor3 = numero == 7 && numero > 100 ? "Lucas" : "Willian";
 
-        //if (cor == Cor.Amarelo && numero < 0)
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //}
-
-
-        //if (cor == Cor.Amarelo && numero >= 0 && ativo)
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //}
-
-
-        //if (cor == Cor.Vermelho || numero > 0)
-        //{
-        //    Console.WriteLine("Login efetuado com sucesso!");
-        //}
-
-
-        //Informações importante: se vc tiver usando o and todas as outras condicoes devem ser verdadeira para
-        //entrar dentro do and e o or precisa apenas de uma condiçao verdadeira para entrar dentro do if
-
-        //string mensagemDeErro = null;
-
-        //if (mensagemDeErro is not null && mensagemDeErro.ToUpper().Equals("LUCAS"))
-        //{
-        //    Console.WriteLine("Entrous");
-        //}
-
-        //Console.WriteLine("View :)");
-
-
-        //No or e quase a mesma coisa ele precisa de apenas 1 valor sendo true se o primeiro for true ele ja via entrar
-        
-                         //|-- td isso de ser true--|
-        if (numero == 0 && (saldo > 100.0 || ativo ))
-        {
-            Console.WriteLine("Entrou dois");
-        }
-
-
-        //por questoes de manutencao de codigo é essencial tomar cuidado com quantidades exageradas de condiciooes dentro dos paramtros
+        Console.WriteLine(autor);
 
     }
 }
--- Rocketseat/C#/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Rocketseat/C#/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cee9dfdfb70b2e231b2c5c2ffedcfab21fe1cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe1e91bcca93754e61c5c9bae92219a63e3a7f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
