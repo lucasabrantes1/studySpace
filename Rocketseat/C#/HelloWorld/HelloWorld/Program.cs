f1da7d31c90ba37cc0cebaa3783619e864d34937
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-10-28 20:04:33 -0300

    O que é o .NET ?

--- Rocketseat/C#/HelloWorld/HelloWorld/Program.cs
+++ Rocketseat/C#/HelloWorld/HelloWorld/Program.cs
@@ -5,26 +5,33 @@ class Program
 {
     static void Main()
     {
-        /*                                        Return
-        * O return também tem como finalidade nos loopings de parar assim como o break a diferença e que um break so pode
-        * se usado dentro de um looping ou switch mas no switch e uma exceção o return podemos usar dentro de funções também
+        /*                                        O que é .NET ?
+        * C# é a linguagen de programação enquanto .NET vai reunir composta de várias blibliotecas uma plataforma. imagine
+        * que precisamos criar uma  aplicação podemos usar  blibliotecas ja criadas com o .net ao invez de criar do zero com c#
+        * ele é composto por variaz libs que na hora de criar nossa aplicação e so escolher o tipo de projeto.
+        * 
+        * O .NET Foi criado pela microsoft mas é open source e gratuita
+        * 
+        * 
+        * Funcionalidades do .NET: código assincrono, funções lambdas, ele tem um proprio coletor denominado pela sigla gc(garbage collector)
+        * e link, que é meio que uma linguagem parecida com banco de dados.
+        * 
+        *
+        todo mes de novembro existe uma nova versão do .net, deve ter atenção pois nos anos impares eles lançam versões LTS, que a 
+        microsoft vai dar suporte a essa versão por três anos, nos anos pares são as versões STS, que a microsoft vai dar suporte por apenas
+        18 meses.
+
+        IMPARES LTS = 3 ANOS
+        PARES STS = 18 MESES
+
+        História do .NET:
+         A microsoft em 2002 lançou esse conjunto de blibliotecas, com o nome de .net framework porem essa plataforma so
+        executa em ambientes windows (2002), ja em 2014 ela lançou a plataforma .net core que dava suporte ao .net alem de
+        ser open source e ser multi plataforma. 
+
+
         */
 
-        var texto = Teste(5);
-        Console.WriteLine(texto);
-    }
 
-    static string Teste( int numero)
-    {   
-
-        Console.WriteLine("Teste 1");
-        if (numero == 5)
-        {
-            Console.WriteLine("Teste 2");
-            return "Lucas";
-
-        }
-        Console.WriteLine("Teste 3");
-        return "Wellisons";
     }
 }
