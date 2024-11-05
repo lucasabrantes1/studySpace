bbf2471b70918fb2e89aafe3d8763cca6093a1f7
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-11-04 21:25:17 -0300

    Como passar valores pela rota

--- Rocketseat/CSharp/CriandoUmaApiEmDotNet/MyFirstApi/obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
+++ Rocketseat/CSharp/CriandoUmaApiEmDotNet/MyFirstApi/obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67412bea9bcf147c0a73a9b61523c9ac0c626185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72f997c18477d3c9a8047a66f77318c5c126632")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
